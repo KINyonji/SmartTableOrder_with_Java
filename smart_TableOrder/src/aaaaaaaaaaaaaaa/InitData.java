c2d07be8dbc26c5516be15f463fde68aed3745e9
KINyonji <73460065+KINyonji@users.noreply.github.com>
2021-01-17 18:21:24 +0900

    commit

---
+++ smart_TableOrder/src/aaaaaaaaaaaaaaa/InitData.java
@@ -0,0 +1,11 @@
+package aaaaaaaaaaaaaaa;
+
+public class InitData {
+	public static String ip = "192.168.0.90";
+	public static void setIp(String ip) {
+		//  192.168.1.36 바나프레소 안정민
+		InitData.ip=ip;
+	}
+}
+
+
